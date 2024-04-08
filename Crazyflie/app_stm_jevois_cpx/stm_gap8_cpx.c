07ebcfc6a5d68f78cd08bd70dbe5701815f188ab
Residualstress <92587824+Residualstress@users.noreply.github.com>
2024-04-08 15:45:22 +0800

    Add files via upload

---
+++ Crazyflie/app_stm_jevois_cpx/stm_gap8_cpx.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "app.h"
+
+#include "cpx.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define DEBUG_MODULE "APP"
+#include "debug.h"
+
+// Callback that is called when a CPX packet arrives
+static void cpxPacketCallback(const CPXPacket_t* cpxRx);
+
+
+void appMain() {
+  DEBUG_PRINT("Hello! I am the stm_gap8_cpx app\n");
+
+  // Register a callback for CPX packets.
+  // Packets sent to destination=CPX_T_STM32 and function=CPX_F_APP will arrive here
+  cpxRegisterAppMessageHandler(cpxPacketCallback);
+
+  while(1) {
+    vTaskDelay(M2T(3000));
+    DEBUG_PRINT("waiting for data \n");
+
+  }
+}
+
+static void cpxPacketCallback(const CPXPacket_t* cpxRx) {
+  //DEBUG_PRINT("Oil palm location is (%u, %u)\n", cpxRx->data[0], cpxRx->data[1]);
+  DEBUG_PRINT("Oil palm location is (%u)\n", cpxRx->data[0]);
+}
