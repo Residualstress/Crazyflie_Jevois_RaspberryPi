d4551f9117183f3f569e63b43a1c2c51cd2cdf2f
Residualstress <92587824+Residualstress@users.noreply.github.com>
2025-01-03 16:20:15 +0800

    Update AoHuaTest.c

--- Crazyflie/AoHuaTest.c
+++ Crazyflie/AoHuaTest.c
@@ -25,26 +25,30 @@ typedef struct {
 typedef struct {
     double x;
     double y;
+    double z;
 } Velocity;
 
-static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z)
+static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float vz)
 {
-  setpoint->mode.z = modeAbs;
-  setpoint->position.z = z;
+  //setpoint->mode.z = modeAbs;
+  //setpoint->position.z = z;
   
   setpoint->mode.x = modeVelocity;
   setpoint->mode.y = modeVelocity;
+  setpoint->mode.z = modeVelocity;
   setpoint->velocity.x = vx;
   setpoint->velocity.y = vy;
-  
+  setpoint->velocity.z = vz; 
   setpoint->velocity_body = true;
 }
 
-static const float height_takeoff = 0.5f;
-static const float height_land = 0.2f;
-static const float distance_x = 1.0f;
-static const float distance_y = 1.0f;
+static const float height_takeoff = 0.4f;
+static const float height_land = 0.15f;
+static const float distance_x = 1.98f;
+static const float distance_y = 0.14f;
 static const double velMax = 0.15f;
+static const double BOX_LIMIT_high = 0.5f;
+static const double BOX_LIMIT_down = 0.3f;
 static Velocity velocity = {0, 0};
 
 
@@ -63,18 +67,19 @@ bool is_at_target(Coordinate target) {
     double tolerance = 0.05;
     logVarId_t idX = logGetVarId("stateEstimate", "x");
     logVarId_t idY = logGetVarId("stateEstimate", "y");
-    
+    logVarId_t idZ = logGetVarId("stateEstimate", "z");
     double postiion_x = logGetFloat(idX);
     double postiion_y = logGetFloat(idY);
-
+    double postiion_z = logGetFloat(idZ);
     double error_x = postiion_x - target.x;
-    if (fabs(error_x) <= tolerance) {
-    	DEBUG_PRINT("X position is satisfied, distance is: %.2f ...\n", error_x);
-        return true;  // Current coordinates are close enough to the target
+    double error_y = postiion_y - target.y;
+    if (fabs(error_x) <= tolerance && fabs(error_y) <= tolerance) {
+    	DEBUG_PRINT("position is satisfied, distance is: %.2f ...\n", error_x);
+        //return true;  // Current coordinates are close enough to the target
     } else{
     
      if (fabs(postiion_x - target.x) > tolerance) {
-    		DEBUG_PRINT("X position isn't satisfied ...\n");
+    		//DEBUG_PRINT("X position isn't satisfied ...\n");
     		if ((target.x-postiion_x)>0){
     			velocity.x = velMax;
     		} else {
@@ -82,9 +87,10 @@ bool is_at_target(Coordinate target) {
     		}
      } else{
     	velocity.x = 0;
-     }   
+     }
+        
      if (fabs(postiion_y - target.y) > tolerance){
-      		DEBUG_PRINT("Y position isn't satisfied ...\n");	
+      		//DEBUG_PRINT("Y position isn't satisfied ...\n");	
     		if ((target.y-postiion_y)>0){
     			velocity.y = velMax;
     		} else {
@@ -94,15 +100,25 @@ bool is_at_target(Coordinate target) {
      } else {
     	velocity.y = 0;
     	}	
-      return false; // Current coordinates are not at the target
+      //return false; // Current coordinates are not at the target
      }
-
+     
+     if (postiion_z > BOX_LIMIT_high){	
+    	velocity.z = -velMax*2;
+    	DEBUG_PRINT("down ...\n"); 	       	
+     } else if (postiion_z < BOX_LIMIT_down) {
+	velocity.z = velMax*2;
+	DEBUG_PRINT("up ...\n");     	
+    	} 	
+     return false; // Current coordinates are not at the target
+     
 }
 
 
 static void Move_to_target(){
 	static setpoint_t setpoint;
-	setHoverSetpoint(&setpoint, velocity.x, velocity.y, height_takeoff);
+	DEBUG_PRINT("Y velocity is: %.2f ...\n", velocity.z);
+	setHoverSetpoint(&setpoint, velocity.x, velocity.y, velocity.z);
         commanderSetSetpoint(&setpoint, 3);
 }
 
@@ -111,7 +127,7 @@ void appMain()
   static setpoint_t setpoint;
   Coordinate target_Coordinate = {distance_x, distance_y}; 
   
-  vTaskDelay(M2T(10000));
+  vTaskDelay(M2T(5000));
   
   paramVarId_t idPositioningDeck = paramGetVarId("deck", "bcFlow2");
 
@@ -132,10 +148,10 @@ void appMain()
         while (1) {
 
         	if (is_at_target(target_Coordinate)){
-        	   break;
+        	   DEBUG_PRINT("bouncing!\n");
         	}
         	Move_to_target();
-        	DEBUG_PRINT("X velocity is %.2f, Y velocity is %.2f ...\n\n", velocity.x, velocity.y);
+        	//DEBUG_PRINT("X velocity is %.2f, Y velocity is %.2f ...\n\n", velocity.x, velocity.y);
 		vTaskDelay(M2T(10));
 	}
 	
