f32ab50823b9605f7fa34949f7e48910b7c3dc6c
Residualstress <92587824+Residualstress@users.noreply.github.com>
2025-01-03 14:14:00 +0800

    Update AoHuaTest.c

--- Crazyflie/AoHuaTest.c
+++ Crazyflie/AoHuaTest.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <math.h>
 #include "app.h"
 
 #include "commander.h"
@@ -16,26 +16,37 @@
 
 #define DEBUG_MODULE "TAKEOFF"
 
-static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate)
+
+typedef struct {
+    double x;
+    double y;
+} Coordinate;
+
+typedef struct {
+    double x;
+    double y;
+} Velocity;
+
+static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z)
 {
   setpoint->mode.z = modeAbs;
   setpoint->position.z = z;
-
-
-  setpoint->mode.yaw = modeVelocity;
-  setpoint->attitudeRate.yaw = yawrate;
-
-
+  
   setpoint->mode.x = modeVelocity;
   setpoint->mode.y = modeVelocity;
   setpoint->velocity.x = vx;
   setpoint->velocity.y = vy;
-
+  
   setpoint->velocity_body = true;
 }
 
-static const float height_sp = 0.5f;
-static const float height_down = 0.2f;
+static const float height_takeoff = 0.5f;
+static const float height_land = 0.2f;
+static const float distance_x = 1.0f;
+static const float distance_y = 1.0f;
+static const double velMax = 0.15f;
+static Velocity velocity = {0, 0};
+
 
 void appMainTask(void *param)
 {
@@ -46,14 +57,62 @@ void appMainTask(void *param)
 
   }
 
+
+bool is_at_target(Coordinate target) {
+    // Compare each coordinate component with a tolerance (e.g., 0.1)
+    double tolerance = 0.05;
+    logVarId_t idX = logGetVarId("stateEstimate", "x");
+    logVarId_t idY = logGetVarId("stateEstimate", "y");
+    
+    double postiion_x = logGetFloat(idX);
+    double postiion_y = logGetFloat(idY);
+
+    double error_x = postiion_x - target.x;
+    if (fabs(error_x) <= tolerance) {
+    	DEBUG_PRINT("X position is satisfied, distance is: %.2f ...\n", error_x);
+        return true;  // Current coordinates are close enough to the target
+    } else{
+    
+     if (fabs(postiion_x - target.x) > tolerance) {
+    		DEBUG_PRINT("X position isn't satisfied ...\n");
+    		if ((target.x-postiion_x)>0){
+    			velocity.x = velMax;
+    		} else {
+			velocity.x = -velMax;    	
+    		}
+     } else{
+    	velocity.x = 0;
+     }   
+     if (fabs(postiion_y - target.y) > tolerance){
+      		DEBUG_PRINT("Y position isn't satisfied ...\n");	
+    		if ((target.y-postiion_y)>0){
+    			velocity.y = velMax;
+    		} else {
+			velocity.y = -velMax;    	
+    		}    	
+    	
+     } else {
+    	velocity.y = 0;
+    	}	
+      return false; // Current coordinates are not at the target
+     }
+
+}
+
+
+static void Move_to_target(){
+	static setpoint_t setpoint;
+	setHoverSetpoint(&setpoint, velocity.x, velocity.y, height_takeoff);
+        commanderSetSetpoint(&setpoint, 3);
+}
+
 void appMain()
 {
   static setpoint_t setpoint;
-
-  vTaskDelay(M2T(3000));
+  Coordinate target_Coordinate = {distance_x, distance_y}; 
   
-//  logVarId_t logIdStateEstimateZ = logGetVarId("stateEstimate", "z");
-//  uint16_t testz = logGetUint(logIdStateEstimateZ);
+  vTaskDelay(M2T(10000));
+  
   paramVarId_t idPositioningDeck = paramGetVarId("deck", "bcFlow2");
 
 
@@ -62,73 +121,38 @@ void appMain()
   uint8_t positioningInit = paramGetUint(idPositioningDeck);
 
   if (positioningInit) {
-    float height = height_sp;
-    float height_land = height_down;
-    float velFront = 0;
-    float velSide = 0;
       if (1) {
         DEBUG_PRINT("Waiting for the client ...\n");
-      	vTaskDelay(M2T(5000));
+      	vTaskDelay(M2T(3000));
+      	
       	DEBUG_PRINT("Taking off ...\n");
- 	int k = 700; // Initialize counter
- 	DEBUG_PRINT("k is %d now\n", k);
- 
-        while (k < 1000) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }
+        vTaskDelay(M2T(10));
         
- 	DEBUG_PRINT("k is %d now\n", k);
- 	DEBUG_PRINT("Falling ...\n");
-
-        while (k < 1200) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height_land, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }	
-
-        while (k < 1400) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }	
-
-        while (k < 1600) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height_land, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }	
-
-        while (k < 1800) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }	
-
-        while (k < 2000) {
-        	vTaskDelay(M2T(10));
-        	setHoverSetpoint(&setpoint, velFront, velSide, height_land, 0);
-        	commanderSetSetpoint(&setpoint, 3);
-        	k++;
-        }	
         
- 
+        while (1) {
+
+        	if (is_at_target(target_Coordinate)){
+        	   break;
+        	}
+        	Move_to_target();
+        	DEBUG_PRINT("X velocity is %.2f, Y velocity is %.2f ...\n\n", velocity.x, velocity.y);
+		vTaskDelay(M2T(10));
+	}
 	
-        
-        DEBUG_PRINT("k is %d now\n", k);
-        DEBUG_PRINT("Landing ...\n");
+	
+	DEBUG_PRINT("Mission complete\n");
+        int k = 0;
+        while (k < 300){
+        	vTaskDelay(M2T(10));
+        	setHoverSetpoint(&setpoint, 0, 0, height_land);
+        	commanderSetSetpoint(&setpoint, 3);
+        	k++;    
+        }
         while (1) {
         	vTaskDelay(M2T(10));     
         	memset(&setpoint, 0, sizeof(setpoint_t));
         	commanderSetSetpoint(&setpoint, 3);
-        }     
-        
+        }        
       }
   } else {
     DEBUG_PRINT("No flow deck installed ...\n");
