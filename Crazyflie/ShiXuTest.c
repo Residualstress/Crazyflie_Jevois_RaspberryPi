863be8a5e3f0b376b453cedcb51dedc8fe91408b
Residualstress <92587824+Residualstress@users.noreply.github.com>
2025-01-17 16:27:24 +0800

    Update ShiXuTest.c

--- Crazyflie/ShiXuTest.c
+++ Crazyflie/ShiXuTest.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <math.h>
 #include "app.h"
-
+#include "cpx.h"
 #include "commander.h"
 
 #include "FreeRTOS.h"
@@ -13,6 +13,7 @@
 
 #include "log.h"
 #include "param.h"
+
 
 #define DEBUG_MODULE "TAKEOFF"
 
@@ -50,10 +51,10 @@ static void cpxPacketCallback(const CPXPacket_t* cpxRx);
 
 static const float height_takeoff = 0.5f;
 static const float height_land = 0.15f;
-static const float distance_x = 3.5f;
+static const float distance_x = 3.6f;
 static const double velMax = 0.15f;
 static Velocity velocity = {0, 0};
-
+static float obstacle = 0;
 
 void appMainTask(void *param)
 {
@@ -65,6 +66,18 @@ void appMainTask(void *param)
   }
 
 
+double drone_speed(double distance, double v_min, double v_max, double midpoint, double steepness) {
+    double normalized_distance = 1.0 / (1.0 + exp(-steepness * (fabs(distance) - midpoint)));
+    double speed = v_min + (v_max - v_min) * normalized_distance;
+    
+    // 关于原点对称
+    if (distance < 0) {
+        speed = -speed;
+    }
+
+    return speed;
+}
+
 bool is_at_target(Coordinate target) {
     // Compare each coordinate component with a tolerance (e.g., 0.1)
     double tolerance = 0.05;
@@ -73,37 +86,18 @@ bool is_at_target(Coordinate target) {
     
     double postiion_x = logGetFloat(idX);
     double postiion_y = logGetFloat(idY);
-
-    double error_x = postiion_x - target.x;
-    if (fabs(error_x) <= tolerance) {
-    	//DEBUG_PRINT("X position is satisfied, distance is: %.2f ...\n", error_x);
+    double distance2_x =  target.x - postiion_x;
+    double distance2_y =  target.y - postiion_y; 	
+	
+    if (fabs(postiion_x - target.x) <= tolerance &&
+        fabs(postiion_y - target.y) <= tolerance) {
+        //DEBUG_PRINT("point arrived, position is  (%.2f, %.2f)\n", (double)postiion_x, (double)postiion_y);
         return true;  // Current coordinates are close enough to the target
-    } else{
-    
-     if (fabs(postiion_x - target.x) > tolerance) {
-    		//DEBUG_PRINT("X position isn't satisfied ...\n");
-    		if ((target.x-postiion_x)>0){
-    			velocity.x = velMax;
-    		} else {
-			velocity.x = -velMax;    	
-    		}
-     } else{
-    	velocity.x = 0;
-     }   
-     if (fabs(postiion_y - target.y) > tolerance){
-      		//DEBUG_PRINT("Y position isn't satisfied ...\n");	
-    		if ((target.y-postiion_y)>0){
-    			velocity.y = velMax;
-    		} else {
-			velocity.y = -velMax;    	
-    		}    	
-    	
-     } else {
-    	velocity.y = 0;
-    	}	
-      return false; // Current coordinates are not at the target
-     }
-
+    } else{ 
+        velocity.x = drone_speed(distance2_x, 0.05, 0.15, 0.1, 2);	
+        velocity.y = drone_speed(distance2_y, 0.05, 0.15, 0.1, 2);
+        return false;
+        } 
 }
 
 
@@ -117,7 +111,7 @@ void appMain()
 {
   static setpoint_t setpoint;
   Coordinate target_Coordinate = {distance_x, 0}; 
-  vTaskDelay(M2T(10000));
+  vTaskDelay(M2T(5000));
   
   
   paramVarId_t idPositioningDeck = paramGetVarId("deck", "bcFlow2");
@@ -131,16 +125,27 @@ void appMain()
   cpxRegisterAppMessageHandler(cpxPacketCallback);	
   if (positioningInit) {
       if (1) {
-        DEBUG_PRINT("Waiting for the client ...\n");
-      	vTaskDelay(M2T(3000));
+        DEBUG_PRINT("Waiting for the client in 5s...\n");
+      	vTaskDelay(M2T(5000));
       	
       	DEBUG_PRINT("Taking off ...\n");
         vTaskDelay(M2T(10));
-        
+        int j = 0;
+        while (j < 500){
+        	vTaskDelay(M2T(10));
+        	setHoverSetpoint(&setpoint, 0, 0, height_takeoff);
+        	commanderSetSetpoint(&setpoint, 3);
+        	j++;    
+        }       
         
         while (1) {
 
         	if (is_at_target(target_Coordinate)){
+        	   DEBUG_PRINT("End point arrived.\n");
+        	   break;
+        	}
+        	if (obstacle == 1.0f){
+        	   DEBUG_PRINT("Drone is landing.\n");
         	   break;
         	}
         	Move_to_target();
@@ -176,14 +181,14 @@ static void cpxPacketCallback(const CPXPacket_t* cpxRx)
     float   divergence = ((float)raw_x) / 100.0f;
 
     uint8_t raw_y  = cpxRx->data[1];
-    float   obstacle = (float)raw_y;
+    obstacle = (float)raw_y;
 
     DEBUG_PRINT("Divergence: %.2f\n", (double)divergence);
     DEBUG_PRINT("Obstacle parameter: %.2f\n", (double)obstacle);
 
     if(obstacle == 1.0f)
     {
-        DEBUG_PRINT("Drone is landing normally.\n");
+        DEBUG_PRINT("Obstacle detected.\n");
         PositionX = logGetFloat(idX);
         DEBUG_PRINT("PositionX is now: %f deg\n", (double)PositionX);
         PositionY = logGetFloat(idY);
